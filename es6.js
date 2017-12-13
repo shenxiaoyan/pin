c930965684e10ccb4ddd5a58fee897783f30cd35
wenyan <wenyan@51miaoge.com>
2017-12-13 22:39:09 +0800

    init

---
+++ es6.js
@@ -0,0 +1,76 @@
+
+
+// let name="lll";
+// console.log(`name is ${name}`)
+
+
+
+// function hello(name1,name2) {
+//     console.log(name1,name2)
+// }
+// let arr = ['imooc','woniu'];
+// hello.apply(null,arr);
+// hello(...arr)
+
+
+// const obj = {name:'imooc',course:'react'};
+// console.log(Object.keys(obj));//[ 'name', 'course' ]
+// console.log(Object.values(obj));//[ 'imooc', 'react' ]
+// console.log(Object.entries(obj));//[ [ 'name', 'imooc' ], [ 'course', 'react' ] ]
+
+
+
+// const name ='imooc';
+// const obj ={
+//
+//     name,//等于name:name,
+//     [name]:'hello',
+//     hello:function () {
+//
+//     },
+//     hello1(){
+//
+//     }
+// }
+// console.log(obj)
+
+
+
+// 对象
+// const obj = {name:'imooc',course:'React'};
+// const obj2 = {type:'IT',name:'shen'};
+// Object.assign(obj,obj2)
+// console.log({...obj,...obj2,date:'2017'})
+
+// console.log(assign([obj,obj2]))
+//
+// function assign(arr) {
+//
+//     let obj={};
+//
+//     arr.forEach(a=>{
+//         for(var v in a){
+//             obj[v] = a[v]
+//         }
+//     })
+//     return obj
+// }
+
+
+
+// 数组
+
+// const arr = ['hello','imooc'];
+// let arg1 =arr[0]
+// let arg2 =arr[1]
+// let [arg1,arg2] =arr;
+// console.log(arg1,'!',arg2)
+
+
+// const obj = {name:'imooc',course:'react'};
+// const {name} = obj;
+// console.log(name,'!',course)
+
+
+
+// 类
---
+++ src/index.js
@@ -0,0 +1,50 @@
+
+import React from 'react'
+import ReactDom from 'react-dom'
+import { createStore,applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import App from './App'
+// import { counter,addGUN,removeGUN,addGunAsync } from './index.redux'
+import { BrowserRouter,Router,Link } from 'react-router-dom'
+import { counter } from './index.redux'
+
+const store = createStore(counter,applyMiddleware(thunk))
+
+// function render() {
+//     ReactDom.render(<App store = {store} addGUN={addGUN} removeGUN={removeGUN} addGunAsync={addGunAsync} />,document.getElementById('root'))
+// }
+// render();
+// store.subscribe(render)
+function er() {
+    return <h1>二</h1>
+}
+function san() {
+    return <h1>三</h1>
+}
+ ReactDom.render(
+     (<Provider store={store}>
+         <BrowserRouter>
+            <div>
+                <ul>
+                    <li>
+                        <link to="/" />一
+                    </li>
+                    <li>
+                        <link to="/er" />二
+                    </li>
+                    <li>
+                        <link to="/san" />三
+                    </li>
+                </ul>
+                <Router path='/' exact component={App}></Router>
+                <Router path='/er' exact component={er}></Router>
+                <Router path='/san' exact component={san}></Router>
+            </div>
+         </BrowserRouter>
+     </Provider>),
+     document.getElementById('root')
+)
+
+
+
